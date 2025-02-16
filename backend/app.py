0c85f0b566e681c04840700f8d1fb5b592baff1c
Zachary-Kiz <zachary.kizell@gmail.com>
2025-02-16 11:08:51 -0500

    Prep for deployment

--- backend/app.py
+++ backend/app.py
@@ -6,11 +6,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-user = os.getenv('MONGODB_USER')
-pwd = os.getenv('MONGODB_PASS')
-name = os.getenv('CLUST_NAME')
-
-client = MongoClient(f'mongodb+srv://{user}:{pwd}@cluster0.kl2j8.mongodb.net/?retryWrites=true&w=majority&appName={name}')
+url = os.getenv('MONGO_URL')
+client = MongoClient(f'{url}')
 collection = client['movieDatabase']['movies']
 
 @app.route("/", methods=['GET'])
